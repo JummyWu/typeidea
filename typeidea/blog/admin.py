993c522e5dcb73b66c0438b6c0351018f16a1410
jummy <jummy@localhost.localdomain>
2018-01-11 22:09:37 +0800

    修改了blog/model数据库，增加了admin的编辑

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -4,17 +4,41 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from .models import Post, Category, Tag
+from typeidea.custom_site import custom_site
 
-@admin.register(Post)
+@admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title','category','status','owner','created_time']
+    list_display_links = ['category','status']
 
+    list_filter = ['category','owner']
+    search_fields = ['title','category__name','owner__username']
+    save_on_top = True
+    show_full_result_count = False
+    
+    actions_on_top = True
+    actions_on_bottom = True
+    date_hierarchy = 'created_time'
+    list_editable = ('title',)
 
-@admin.register(Category)
+    #编辑页面
+    save_on_top = True
+    #exclude = ('owner',)#不展示字段:w
+    fieldsets = (  # 跟fields互斥
+        ('基础配置', {
+            'fields': (('category', 'title'), 'content')
+        }),
+        ('高级配置', {
+            'classes': ('collapse', 'addon'),
+            'fields': ('tags', ),
+        }),
+    )
+
+@admin.register(Category, site=custom_site)
 class CategoryAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Tag)
+@admin.register(Tag, site=custom_site)
 class TagAdmin(admin.ModelAdmin):
     pass
--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -18,9 +18,12 @@ class Category(models.Model):
     owner = models.ForeignKey(User, verbose_name="作者")
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
+    def __unicode__(self):
+        return self.name
+
     class Meta:
         verbose_name = verbose_name_plural = '分类'
-
+    
 
 class Tag(models.Model):
     STATUS_ITEMS = (
