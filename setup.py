76d58bf832b45c535930ba43b658227afa27bbe2
jummy <929440925@qq.com>
2018-01-31 17:46:24 +0800

    自动化部署

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ print(packages)
 
 setup(
     name='typeidea',
-    version='0.1',
+    version='0.5',
     decription='Blog System base on Django',
     author='jummy',
     author_email='929440925@qq.com',
@@ -19,6 +19,20 @@ setup(
     include_package_data=True,
     install_requires=[
         'django==1.11.8',
+        'django-autocomplete-light==3.2.10',
+        'hiredis==0.2.0',
+        'redis==2.10.6',
+        'django-ckeditor==5.3.1',
+        'django-debug-toolbar==1.9.1',
+        'mysqlclient==1.3.12',
+        'Pillow==5.0.0',
+        'xadmin==0.6.1',
+        'djangorestframework==3.7.7',
+        'django-reversion==2.0.12',
+        'Markdown==2.6.11',
+        'django-redis==4.8.0',
+        'coreapi==2.3.3',
+        'gunicorn==19.7.1',
     ],
     scripts=[
         'typeidea/manage.py',
--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #'raven.contrib.django.raven_compat',
 ]
 
 MIDDLEWARE = [
