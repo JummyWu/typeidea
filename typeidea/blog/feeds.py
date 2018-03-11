6201393d611e1fe7b6a155b0baed61ddc74b6754
jummy <jummy@localhost.localdomain>
2018-03-11 12:55:51 +0800

    添加了RSS功能

---
+++ typeidea/blog/feeds.py
@@ -0,0 +1,53 @@
+# coding:utf-8
+from __future__ import unicode_literals
+
+from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Rss201rev2Feed
+from django.urls import reverse
+
+from blog.models import Post 
+
+class ExtendedRSSFeed(Rss201rev2Feed):
+    mime_type = 'application/xml'
+
+    def root_attributes(self):
+        attrs = super(ExtendedRSSFeed, self).root_attributes()
+        attrs['xmlns:content'] = 'http://purl.org/rss/1.0/modules/content/'
+        return attrs
+
+    def add_item_elements(self,handler,item):
+        super(ExtendedRSSFeed, self).add_item_elements(handler, item)
+        handler.addQuickElement(u'content:encoded',item['content_encoded'])
+
+
+class AllPostRssFeed(Feed):
+    feed_type = ExtendedRSSFeed 
+
+    title = "Jummy"
+    link  = "http://www.jummy.top"
+    author = 'Jummy'
+    description = "Jummy Blog"
+
+    def items(self):
+        return Post.objects.filter(status=0).order_by('-created_time')[:10]
+
+    def item_extra_kwargs(self, item):
+        return {'content_encoded': self.item_content_encoded(item)}
+
+    def item_title(self, item):
+        return item.title 
+
+    def item_description(self, item):
+        return item.html
+
+    def item_author_name(self, item):
+        if (item.author.get_full_name()):
+            return item.author.get_full_name()
+        else:
+            return item.author
+
+    def item_pubdate(self, item):
+        return item.created_time
+
+    def item_content_encoded(self, item):
+        return item.html
